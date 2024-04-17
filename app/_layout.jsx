ea1c99898c421caf76cb717ee7ae2bb814a78778
colinjcason <colinjcason@gmail.com>
2024-04-17 15:33:02 -0700

    first commmit

---
+++ app/_layout.jsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Slot, Stack } from "expo-router";
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
